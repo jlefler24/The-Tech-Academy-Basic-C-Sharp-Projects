bd60415da3c8bd57b90fd339e36e0364f8f5087b
jlefler24 <lefler24@aol.com>
2020-06-12 02:48:31 -0600

    LoopAssignment
    LoopAssignment commit

---
+++ LoopsAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1-10?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isCorrect = number == 4;
+
+
+            do
+            {
+                switch (number)
+                {
+                    case 4:
+                        Console.WriteLine("You guessed 4. You're Correct!");
+                        isCorrect = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You guessed wrong. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isCorrect) ;
+        
+            Console.Read();
+        }
+    }
+}
