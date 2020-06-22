a88713a63d0df03e1c243ef3c0797acd89f9e1ae
jlefler24 <60869396+jlefler24@users.noreply.github.com>
2020-06-22 03:45:01 -0600

    Update
    Update

--- LambdaExpressions/LambdaExpressions/Program.cs
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -17,32 +17,36 @@ namespace LambdaExpressions
                 new Employee {FirstName="Aaron", LastName="B", ID=3},
                 new Employee {FirstName="Dusty", LastName="H", ID=4},
                 new Employee {FirstName="Henry", LastName="K", ID=5},
-                new Employee {FirstName="Cody", LastName="I", ID=6 },
-        
+                new Employee {FirstName="Joe", LastName="E", ID=6 },
+                new Employee {FirstName="Justin", LastName="A", ID=7 },
+                new Employee {FirstName="Duke", LastName="H", ID=8 },
+                new Employee {FirstName="Joe", LastName="M", ID=9 },
+                new Employee {FirstName="Cody", LastName="I", ID=10 },
+
             };
 
-            List<Employee> names = new List<Employee>();
+            List<Employee> joes = new List<Employee>();
             foreach (Employee employee in employees)
             {
-                if (employee.FirstName == "Jordan")
+                if (employee.FirstName == "Joe")
                 {
-                    names.Add(employee);
+                    joes.Add(employee);
                 }
             }
 
-            List<Employee> names2 = employees.FindAll(x => x.FirstName == "Austin").ToList();
+            List<Employee> joes2 = employees.FindAll(x => x.FirstName == "Joe").ToList();
 
             List<Employee> ids = employees.FindAll(x => x.ID > 5).ToList();
 
             Console.WriteLine("Employee list of names generated from foreach loop:");
-            foreach (Employee employee in names)
+            foreach (Employee employee in joes)
             {
                 Console.WriteLine("Employee: {0} {1} {2}", employee.ID, employee.FirstName, employee.LastName);
             }
             Console.ReadLine();
 
-            Console.WriteLine("Employee list of names generated from lambda expression:");
-            foreach (Employee employee in names2)
+            Console.WriteLine("Employee list of joes generated from lambda expression:");
+            foreach (Employee employee in joes2)
             {
                 Console.WriteLine("Employee: {0} {1} {2}", employee.ID, employee.FirstName, employee.LastName);
             }
