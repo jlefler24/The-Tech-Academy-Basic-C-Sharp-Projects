6bcb9f2a26a0f0e34eed2796ab0437fe69ed10a2
jlefler24 <lefler24@aol.com>
2020-07-10 02:29:12 -0600

    Update
    Update

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Range(0, 5)]
+        public int Credits { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public virtual Department Department { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual ICollection<Instructor> Instructors { get; set; }
+    }
+}
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+//namespace ContosoUniversity.Models
+//{
+//    public class Course
+//    {
+//        [DatabaseGenerated(DatabaseGeneratedOption.None)] //Allows us to enter PK rather than DB generated.
+//        public int CourseID { get; set; }
+//        public string Title { get; set; }
+//        public int Credits { get; set; }
+
+//        public virtual ICollection<Enrollment> Enrollments { get; set; }// Enrollments property is a navigation property.
+//                                                                        // A course entity (ONE) can be related to (MANY) Enrollment entities.
+//    }
+//}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace ContosoUniversity.Models
+//{
+    
+//    public enum Grade
+//    {
+//        A, B, C, D, F
+//    }
+//    public class Enrollment
+//    {
+//        public int EnrollmentID { get; set; }
+//        public int CourseID { get; set; }
+//        public int StudentID { get; set; } //FOREIGN KEY, CORRESPONDING TO NAVIGATION PROPERTY: Course
+//                                            // An (MANY)Enrollment entity is associated with ONE Course entity.
+//        public Grade? Grade { get; set; }
+
+//        public virtual Course Course { get; set; }
+//        public virtual Student Student { get; set; }
+//    }
+
+//    // Entity Framework interprets a property as a foreign key property if it's named 
+//    // <navigation property name> <primary key property name> 
+//    // ex: FK: StudentID, as it is the PK for the Student enitity
+//    // ex: FK CourseID, as it is the PK for the Course entity
+//}
